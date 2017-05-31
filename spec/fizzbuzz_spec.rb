51bb372358395e1a6ddeadb76c77ff6510ea8203
Verity <vhonebon@gmail.com>
2017-05-31 21:54:24 +0100

    All fizzbuzz tests written

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,17 @@
 require 'fizzbuzz'
 describe "fizzbuzz" do 
-	it "returns 'fizz' when passed 3" do
+	it "returns 'fizzbuzz' when divisible by 3 and 5" do
+		expect(fizzbuzz(15)).to eq "fizzbuzz"
+	end
+	it "returns 'fizz' when divisible by 3" do
 		expect(fizzbuzz(3)).to eq "fizz"
 	end
+	it "returns 'buzz' when divisible by 5" do
+		expect(fizzbuzz(5)).to eq "buzz"
+	end
+	it "returns number when not divisble by 3 or 5" do
+		expect(fizzbuzz(11)).to eq 11
+	end
 end
+
+
